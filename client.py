f0256beb2e773ebb3f0b64d583e7d6529563fa44
Ron Mordo <ronmordo1@outlook.com>
2024-06-28 20:18:24 +0300

    Functioning chat app with different rooms and clients, still thare are Bugs to fix.

--- client.py
+++ client.py
@@ -9,34 +9,66 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9999))
 
 
+input_lock = threading.Lock()
+input_priority = True
+
+
+# Menu
+def menu(connected=False):
+    global input_priority
+    with input_lock:
+        if input_priority:
+            command = "!" + input("Would you like to [C]reate a room, [J]oin a room, [L]ist rooms, or [Q]uit? ").lower()
+            client.send(command.encode('ascii'))
+            input_priority = False
+    if not connected:
+        write()
+
+
 # Listening to Server and Sending Nickname
 def receive():
+    global input_priority
     while True:
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
             message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
+            if message == '!NICK':
                 client.send(nickname.encode('ascii'))
+            elif message == '!MENU':
+                input_priority = True
+                menu(True)
+            elif message == '!QUIT':
+                print("Quitting...")
+                client.close()
+                break
             else:
                 print(message)
         except:
             # Close Connection When Error
-            print("An error occured!")
+            print("An error occurred!")
             client.close()
             break
 
 
 # Sending Messages To Server
 def write():
+    global input_priority
     while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        with input_lock:
+            if not input_priority:
+                message = input('')
+                if message.startswith('!'):
+                    client.send(message.encode('ascii'))
+                else:
+                    message = '{}: {}'.format(nickname, message)
+                    client.send(message.encode('ascii'))
 
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-write_thread = threading.Thread(target=write)
+write_thread = threading.Thread(target=menu)
 write_thread.start()
+
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 # Connection Data
 host = '127.0.0.1'
@@ -15,11 +16,93 @@ server.listen()
 clients = []
 nicknames = []
 
+# Dictionary For Rooms
+rooms = {}
+
+
+MENU_MESSAGE = '!MENU'
+
 
 # Sending Messages To All Connected Clients
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+def broadcast(message, sender):
+    for room in rooms:
+        if sender in rooms[room]:
+            for client in rooms[room]:
+                if client != sender:
+                    client.send(message)
+
+
+def create_room(client):
+    client.send('Enter the name of the room you would like to create: '.encode('ascii'))
+    room_name = client.recv(1024).decode('ascii')
+    room_name = room_name.split(':')[1].strip()
+    if room_name in rooms:
+        client.send(f'{MENU_MESSAGE}'.encode('ascii'))
+    else:
+        rooms[room_name] = []
+        rooms[room_name].append(client)
+        client.send(f'Room {room_name} created and you have joined!'.encode('ascii'))
+
+
+def join_room(client):
+    client.send('Enter the name of the room you would like to join: '.encode('ascii'))
+    room_name = client.recv(1024).decode('ascii')
+    room_name = room_name.split(':')[1].strip()
+    if room_name in rooms:
+        rooms[room_name].append(client)
+        client.send(f'Joined room {room_name}!'.encode('ascii'))
+        broadcast(f'{nicknames[clients.index(client)]} joined the room!'.encode('ascii'), client)
+    else:
+        client.send(f'{MENU_MESSAGE}'.encode('ascii'))
+
+
+def list_rooms(client):
+    client.send('Rooms: '.encode('ascii'))
+    for room in rooms:
+        client.send(f'{room}, '.encode('ascii'))
+    time.sleep(1)
+    client.send(f'{MENU_MESSAGE}'.encode('ascii'))
+
+
+def quit_program(client):
+    index = clients.index(client)
+    nickname = nicknames[index]
+    for room in rooms:
+        if client in rooms[room]:
+            rooms[room].remove(client)
+            broadcast(f'{nickname} left the room!'.encode('ascii'), client)
+    clients.remove(client)
+    client.send('You have left the server!'.encode('ascii'))
+    time.sleep(1)
+    client.send('!QUIT'.encode('ascii'))
+    client.close()
+    nicknames.remove(nickname)
+
+
+def leave_room(client):
+    for room in rooms:
+        if client in rooms[room]:
+            rooms[room].remove(client)
+            broadcast(f'{nicknames[clients.index(client)]} left the room!'.encode('ascii'), client)
+        client.send('You have left the room!'.encode('ascii'))
+        time.sleep(1)
+        client.send(f'{MENU_MESSAGE}'.encode('ascii'))
+
+
+# Handling Commands From Clients
+def handle_command(client, command):
+    if command == '!c':
+        create_room(client)
+    elif command == '!j':
+        join_room(client)
+    elif command == '!l':
+        list_rooms(client)
+    elif command == '!q':
+        quit_program(client)
+    elif command == '!LEAVE':
+        leave_room(client)
+    else:
+        client.send('Invalid command!'.encode('ascii'))
 
 
 # Handling Messages From Clients
@@ -28,15 +111,18 @@ def handle(client):
         try:
             # Broadcasting Messages
             message = client.recv(1024)
-            broadcast(message)
+            if message.decode('ascii').startswith('!'):
+                handle_command(client, message.decode('ascii'))
+            else:
+                broadcast(message, client)
         except:
             # Removing And Closing Clients
             index = clients.index(client)
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'), client)
+            nicknames.remove(nickname)
             clients.remove(client)
             client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
             break
 
 
@@ -48,15 +134,14 @@ def receive():
         print("Connected with {}".format(str(address)))
 
         # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
+        client.send('!NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+        client.send('You are connected to server!'.encode('ascii'))
 
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
@@ -65,3 +150,4 @@ def receive():
 
 print("Server is listening...")
 receive()
+
